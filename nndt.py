67e36351358a58578d5533102382191711cfaf41
Pebaz <pebazium@gmail.com>
2020-09-22 16:10:09 -0400

    save

--- nndt.py
+++ nndt.py
@@ -1,29 +1,56 @@
+class _VariableLength:
+    "Mixin class to ensure that staticly sized classes don't get indexed."
+
+    def __len__(self):
+        "Length is equal to shape plus the shape of all contained values."
+        return self.SHAPE + sum(len(v) for v in self.value)
+
+class NNDTException(Exception):
+    "Base Exception for NNDT library."
+
+class NNDTIndexException(NNDTException):
+    def __init__(self, class_, key):
+        self.class_ = class_
+        self.key = key
+
+    def __str__(self):
+        return (
+            f'{self.class_.__name__} has a static SHAPE of {self.class_.SHAPE}, '
+            f'cannot be set to {self.key}'
+        )
+
 class NNDTType(type):
+    """
+    Metaclass for a range of Neural Network-aware Data Types.
+
+    Supports variable shaped NN input layers by allowing indexing the class
+    itself. For instance, an Int has a shape of 1 since it is a single node that
+    gets passed to the NN. However, an Array needs to be able to support any
+    number of nodes based on usage. To accomplish this, you can index the Array
+    class directly to create a brand new type that has shape of N.
+    """
     nndt_types = {}
 
-    def __new__(cls, name, bases, dict_):
-        print(1, cls)
-        print(2, name)
-        print(3, bases)
-        print(4, dict_)
-        
-        cls.nndt_types[name] = (bases, dict_)
-
-        cls.name = name
-        cls.bases = bases
-        cls.dict_ = dict_
-
-        new_type = super().__new__(cls, name, bases, dict_)
-        cls.nndt_types[new_type] = name
-        return new_type
+    def __new__(cls, name, bases, dict_):        
+        cls.nndt_types[name] = bases, dict_
+        return super().__new__(cls, name, bases, dict_)
 
     def __getitem__(self, key):
+        "Create unique class (not object) with custom `SHAPE`."
+
         bases, dict_ = self.nndt_types[self.__name__]
 
-        #result = super().__new__(self.__class__, self.name, self.bases, self.dict_)
-        result = super().__new__(self.__class__, self.__name__, bases, dict_)
-        result.SHAPE = key
-        return result
+        if _VariableLength not in bases:
+            raise NNDTIndexException(self, key)
+
+        dict_['SHAPE'] = key
+
+        return super().__new__(
+            self.__class__,
+            self.__name__,
+            bases,
+            dict_
+        )
 
     def __str__(self):
         return f'<{self.__name__}[{self.SHAPE}]>'
@@ -31,19 +58,13 @@ class NNDTType(type):
 class NNDT(metaclass=NNDTType):
     SHAPE = 1
 
-    # def __new__(cls, *args, **kwargs):
-    #     return super().__new__(cls, *args, **kwargs)
-
-    # def __init__(self, value):
-    #     self.value = value
-
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
         return f'<{self.__class__.__name__}[{self.SHAPE}] {repr(self.value)}>'
 
-class String(NNDT):
+class String(NNDT, _VariableLength):
     pass
 
 print(5, NNDT)
@@ -52,3 +73,11 @@ print(7, NNDT[5])
 print(8, NNDT[3](100))
 print(9, String[100])
 print(10, String[100]('Foo'))
+
+
+# Array[String[10]]  (array of 1 string of length 10)
+# Array[3, String[10]]  (array of 3 strings of length 10)
+
+# TODO(pebaz): Support Array[3, String[10]] for array of phone numbers
+# TODO(pebaz): This needs to have the right SHAPE and len.
+
