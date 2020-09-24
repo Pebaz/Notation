57c2126ffe78fdd25d934f813bea01f461c9b4b7
Pebaz <pebazium@gmail.com>
2020-09-24 17:43:56 -0400

    Found way to have multiple inputs

--- mod_test.py
+++ mod_test.py
@@ -1,15 +1,29 @@
+from random import randint
 from keras.models import Sequential
 from keras.layers import Dense
 
-model = Sequential([Dense(3, input_shape=(3,))])
+INPUT_NODES = 3
+OUTPUT_NODES = 3
+
+model = Sequential([
+    Dense(OUTPUT_NODES, input_shape=(INPUT_NODES,))
+])
 
 model.compile(loss='mse', optimizer='adam')
 
-input_data = [[i for i in range(3)], [i for i in range(3)]]
+input_data = [[randint(1, 100) for _ in range(3)] for i in range(1000000)]
 output_data = [[i + 1 for i in data] for data in input_data]
 
 print()
 
 model.fit(input_data, output_data)
 
-print([*model.predict([[10, 11, 12]])[0]])
+get_result = lambda prediction: list(prediction[0])
+input_ = [10, 11, 12]
+pred = model.predict([input_])
+
+print()
+
+print(get_result(pred))
+
+
