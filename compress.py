3d8e3105f77ed8824ba0ae2e39e7ae17d2cef129
Pebaz <pebazium@gmail.com>
2020-09-29 17:03:59 -0400

    added documentation

--- compress.py
+++ compress.py
@@ -1,37 +1,47 @@
+"""
+Samuel Wilder 9/29/2020
 
+Obfuscates a given piece of data (in this case a string) by hiding within an ML
+model.
 
+To get that data back out, a string matching the password must be passed in.
 
+All other input returns garbled text.
+"""
 
+DATA = 'this is something that should be kept hidden'
+PASSCODE = 'who me'  # Expected password
 
 from keras.models import Model
 from keras.layers import Input, Dense
 
-
 def create_model(inputs, outputs):
+    "Creates a simple model with 1 input & output layer with the given sizes."
     inp = Input(shape=(inputs,))
     out = Dense(outputs)(inp)
     model = Model(inputs=inp, outputs=out)
     model.compile(loss='mse', optimizer='adam')
     return model
 
-data = 'this is something that should be kept hidden'
-passcode = 'who me'  # Expected password
-
+# Turns a string into a list of ints matching length of input/output layer
 as_layer = lambda s, size: [ord(c) for c in (s + ' ' * (size - len(s)))]
 
-mod = create_model(len(passcode), len(data))
+mod = create_model(len(PASSCODE), len(DATA))
+
+num_iterations = 100000
+
 mod.fit(
     [
-        as_layer(passcode, len(passcode))
-    ] * 100000,
+        as_layer(PASSCODE, len(PASSCODE))
+    ] * num_iterations,
     [
-        as_layer(data, len(data))
-    ] * 100000
+        as_layer(DATA, len(DATA))
+    ] * num_iterations
 )
 
-
+# ------------------------------------------------------------------------------
 result = mod.predict([
-    as_layer('hello', len(passcode))  # Wrong password
+    as_layer('hello', len(PASSCODE))  # Wrong password
 ])[0]
 
 string = [chr(max(0, min(0x110000 - 1, round(c)))) for c in result]
@@ -39,10 +49,13 @@ string = [chr(max(0, min(0x110000 - 1, round(c)))) for c in result]
 print(''.join(string))
 
 
+# ------------------------------------------------------------------------------
 result = mod.predict([
-    as_layer('who me', len(passcode))  # Right password
+    as_layer('who me', len(PASSCODE))  # Right password
 ])[0]
 
 string = [chr(max(0, min(0x110000 - 1, round(c)))) for c in result]
 
 print(''.join(string))
+
+# mod.save('compress')
