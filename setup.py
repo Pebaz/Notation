75d6210e9f3334f884240ce68fc9dc749840e2a4
Pebaz <pebazium@gmail.com>
2020-10-09 17:47:44 -0400

    added MPL 2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name='notation',
 	version='0.1.0',
-	license='MIT',
+	license='MPL2.0',
 	python_requires='>=3.6.0',
 	description='Neural Network-Aware Data Types',
 	long_description=open('README.md').read(),
